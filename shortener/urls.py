3e48d6b0a28316695bc841692cdc262b7227a4ed
[haleema-naseer065] <[haleemanaseer065@gmail.com]>
2025-03-23 00:58:59 +0500

    changed endpoint names

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test-create/', views.create_short_url, name='create_short_url'),
-    path('test-retrieve/<str:shortCode>/', views.retrieve_short_url, name='retireve_short_url'),
-    path('test-delete/<str:shortCode>/', views.delete_short_url, name='delete_short_url'),
-    path('test-update/<str:shortCode>/', views.update_short_url, name='update_short_url'),
-    path('test-stats/<str:shortCode>/', views.stats_short_url, name='stats_short_url'),
+    path('shorten/', views.create_short_url, name='create_short_url'),
+    path('shorten/<str:shortCode>/', views.retrieve_short_url, name='retireve_short_url'),
+    path('shorten/<str:shortCode>/', views.delete_short_url, name='delete_short_url'),
+    path('shorten/<str:shortCode>/', views.update_short_url, name='update_short_url'),
+    path('statistics/<str:shortCode>/', views.stats_short_url, name='stats_short_url'),
 ]
