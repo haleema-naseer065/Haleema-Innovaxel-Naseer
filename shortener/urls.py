6fc43ad4ea45ffc813a20cc68eb18adbd3e986c8
[haleema-naseer065] <[haleemanaseer065@gmail.com]>
2025-03-23 01:21:19 +0500

    combining HTTP methods

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('shorten/', views.create_short_url, name='create_short_url'),
     path('shorten/<str:shortCode>/', views.retrieve_short_url, name='retireve_short_url'),
-    path('shorten/<str:shortCode>/', views.delete_short_url, name='delete_short_url'),
-    path('shorten/<str:shortCode>/', views.update_short_url, name='update_short_url'),
-    path('statistics/<str:shortCode>/', views.stats_short_url, name='stats_short_url'),
+    # path('shorten/<str:shortCode>/', views.delete_short_url, name='delete_short_url'),
+    # path('shorten/<str:shortCode>/', views.update_short_url, name='update_short_url'),
+    # path('statistics/<str:shortCode>/', views.stats_short_url, name='stats_short_url'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -25,19 +25,39 @@ def create_short_url(request):
   except Exception as e:
     return Response({'error':'An unexpected error has occurred,'},status = status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def retrieve_short_url(request,shortCode):
   try:
     short_url = ShortURL.objects.get(shortCode = shortCode)
+  except ShortURL.DoesNotExist:
+    return Response({'error': shortCode + 'does not exist'},status = status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
     short_url.access_count += 1
     short_url.save()
 
     serializer = ShortURLSerializer(short_url)
 
     return Response(serializer.data,status = status.HTTP_200_OK)
+  
+  elif request.method == 'PUT':
+    url = request.data.get('url')
+    if not url:
+      return Response({'error':'URL is required'},status = status.HTTP_400_BAD_REQUEST)
+  
+    short_url = ShortURL.objects.get(shortCode = shortCode)
+    short_url.url = url
+    short_url.updated_at = timezone.now()
+    short_url.save()
 
-  except ShortURL.DoesNotExist:
-    return Response({'error': shortCode + 'does not exist'},status = status.HTTP_404_NOT_FOUND)
+    serializer = ShortURLSerializer(short_url)
+    return Response(serializer.data,status = status.HTTP_200_OK)
+
+  elif request.method == 'DELETE':
+    short_url.delete()
+    return Response(status = status.HTTP_204_NO_CONTENT)
+
+
 
 @api_view(['DELETE'])
 def delete_short_url(request,shortCode):
